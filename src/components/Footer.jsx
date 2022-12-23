83304680a4e4b15bb02cbc862b899892d4eaef5a
Ugiomoh Daniel <ugiomohd@yahoo.com>
2022-12-23 05:33:26 +0100

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,59 +9,57 @@ import {
 
 const Footer = () => {
   return (
-    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
-      <div>
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-5 md:grid-cols-2 gap-4 text-gray-300">
+      <div className="lg:col-span-1 md:col-span-2">
         <h1 className="w-full text-3xl font-bold text-[#ffc300]">Export.</h1>
-        <p>
-          Export is a digital B2B marketplace aiming to be a comprehensive
+        <p className="text-sm text-justify">
+          Export is a digital B2B market aiming to be a comprehensive
           international trade hub for Small and Medium - sized Enterprises
           (SMEs) and their counterparts.
         </p>
-        <div className="flex text-[#ffc300] my-6 md:w-[75%] justify-between">
-          <FaFacebookSquare size={30} />
-          <FaInstagram size={30} />
-          <FaGithubSquare size={30} />
-          <FaTwitterSquare size={30} />
-          <FaDribbbleSquare size={30} />
-        </div>
+        <p className="flex text-[#ffc300] my-6 justify-between">
+          <FaFacebookSquare size={25} />
+          <FaInstagram size={25} />
+          <FaGithubSquare size={25} />
+          <FaTwitterSquare size={25} />
+          <FaDribbbleSquare size={25} />
+        </p>
       </div>
-      <div className="lg:col-span-2 mt-6 flex justify-between">
-        <div>
-          <h6 className="font-medium text-gray-400">Solutions</h6>
-          <ul>
-            <li className="py-2 text-sm">Analytics</li>
-            <li className="py-2 text-sm">Marketting</li>
-            <li className="py-2 text-sm">Commerce</li>
-            <li className="py-2 text-sm">Insights</li>
-          </ul>
-        </div>
-        <div>
-          <h6 className="font-medium text-gray-400">Enterprise</h6>
-          <ul>
-            <li className="py-2 text-sm">Business</li>
-            <li className="py-2 text-sm">Alternate</li>
-            <li className="py-2 text-sm">Income</li>
-            <li className="py-2 text-sm">Multiply</li>
-          </ul>
-        </div>
-        <div>
-          <h6 className="font-medium text-gray-400">Pricing</h6>
-          <ul>
-            <li className="py-2 text-sm">Servicom</li>
-            <li className="py-2 text-sm">Telecom</li>
-            <li className="py-2 text-sm">Practice</li>
-            <li className="py-2 text-sm">Import</li>
-          </ul>
-        </div>
-        <div>
-          <h6 className="font-medium text-gray-400">Community</h6>
-          <ul>
-            <li className="py-2 text-sm">Power</li>
-            <li className="py-2 text-sm">Marketting</li>
-            <li className="py-2 text-sm">Group</li>
-            <li className="py-2 text-sm">Online</li>
-          </ul>
-        </div>
+      <div className="mt-10 text-center">
+        <h1 className="text-[#ffc300] uppercase">Solutions</h1>
+        <ul className="py-4">
+          <li>Analytics</li>
+          <li>Marketting</li>
+          <li>Commerce</li>
+          <li>Insights</li>
+        </ul>
+      </div>
+      <div className="mt-10 text-center">
+        <h1 className="text-[#ffc300] uppercase">Pricing</h1>
+        <ul className="py-4">
+          <li>Business</li>
+          <li>Alternate</li>
+          <li>Income</li>
+          <li>Multiply</li>
+        </ul>
+      </div>
+      <div className="mt-10 text-center">
+        <h1 className="text-[#ffc300] uppercase">Community</h1>
+        <ul className="py-4">
+          <li>Servicom</li>
+          <li>Telecom</li>
+          <li>Practice</li>
+          <li>Import</li>
+        </ul>
+      </div>
+      <div className="mt-10 text-center">
+        <h1 className="text-[#ffc300] uppercase">Enterprise</h1>
+        <ul className="py-4">
+          <li>Power</li>
+          <li>Market</li>
+          <li>Group</li>
+          <li>Online</li>
+        </ul>
       </div>
     </div>
   );
